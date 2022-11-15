ea8a0b748982695f7ebd608163f60c0400b28d1f
Rowan Lea <rowan.lea@codurance.com>
2022-11-15 13:23:54 +0000

    Added outline for gameplay loop acceptance test

--- ConsequencesClientExample/ConsequencesClientExampleTests/GameplayAcceptanceTests.cs
+++ ConsequencesClientExample/ConsequencesClientExampleTests/GameplayAcceptanceTests.cs
@@ -2,5 +2,20 @@
 {
     internal class GameplayAcceptanceTests
     {
+        [Test]
+        public void WhenGameRun_SocketClientConnectCalled()
+        {
+            // Arrange
+            string uri = "ws://0.0.0.0:1234";
+            IThroughput throughput = Substitute.For<IThroughput>();
+            ISocketClient socketClient = Substitute.For <ISocketClient>();
+            GameRunner gameRunner = new GameRunner(throughput, socketClient);
+            
+            // Act
+            gameRunner.Start();
+
+            // Assert
+            socketClient.Received().Connect()
+        }
     }
 }
