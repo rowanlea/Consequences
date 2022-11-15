b4f940226b847199b2f39e28ff79e95c7a4ec080
Rowan Lea <rowan.lea@codurance.com>
2022-11-15 19:06:50 +0000

    Added a concreate implementation of IThroughput and created an actual game to play

---
+++ ConsequencesClientExample/ConsequencesClientExample/InputOutput/ConsoleThroughput.cs
@@ -0,0 +1,17 @@
+﻿namespace ConsequencesClientExample.InputOutput
+{
+    public class ConsoleThroughput : IThroughput
+    {
+        public void OutputToConsole(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        public string TakeUserInput()
+        {
+            string input = "";
+            input += Console.ReadLine();
+            return input;
+        }
+    }
+}
--- ConsequencesClientExample/ConsequencesClientExample/Program.cs
+++ ConsequencesClientExample/ConsequencesClientExample/Program.cs
@@ -1,2 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using ConsequencesClientExample.Game;
+using ConsequencesClientExample.InputOutput;
+using ConsequencesClientExample.Websocket;
+
+IThroughput throughput = new ConsoleThroughput();
+ISocketClient socketClient = new SocketClient();
+GameRunner gameRunner = new GameRunner(throughput, socketClient);
+gameRunner.Start("ws://51.141.52.52:1234");
+
+Console.WriteLine("Press any key to end.");
+Console.ReadKey();
